acab95db259a950f2c17bbac62814d1f69769e5d
tara <tara.tandel@cafebazaar.ir>
2018-10-16 18:24:51 +0330

    add presantation otes

--- ARTest/ViewController.swift
+++ ARTest/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import ARKit
 class ViewController: UIViewController {
-
+    
+    // every AR program should start with an AR session, an ARSCNView has ARSession in its object
     @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addBoxes()
@@ -19,19 +21,18 @@ class ViewController: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        /*
-         The world tracking configuration tracks the device’s orientation and position. It also detects real-world surfaces seen through the device’s camera.
-         */
+
         let config = ARWorldTrackingConfiguration()
+        
         sceneView.session.run(config)
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(true)
-        // I wont resume once we paused
+        // I wont resume once we paused but from ios 12 this feature was added
         sceneView.session.pause()
     }
     
-    func addBox() {
+    func addBoxes(x: Float = 0, y: Float = 0, z: Float = -0.2) {
         //  Create a box shape. 1 Float = 1 meter.
         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         //Create a node. A node represents the position and the coordinates of an object in a 3D space. By itself, the node has no visible content.
@@ -39,19 +40,8 @@ class ViewController: UIViewController {
         //  We can give the node a visible content by giving it a shape. We do this by setting the node’s geometry to the box.
         boxNode.geometry = box
         //  we give our node a position. This position is relative to the camera. Positive x is to the right. Negative x is to the left. Positive y is up. Negative y is down. Positive z is backward. Negative z is forward.
-        boxNode.position = SCNVector3(0, 0, -0.2)
-        // Then we create a scene. This is the SceneKit scene to be displayed in the view. We then add our box node to the root node of the scene. A root node in a scene that defines the coordinate system of the real world rendered by SceneKit.
-        let scene = SCNScene()
-        scene.rootNode.addChildNode(boxNode)
-        sceneView.scene = scene
-    }
-    func addBoxes(x: Float = 0, y: Float = 0, z: Float = -0.2) {
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        
-        let boxNode = SCNNode()
-        boxNode.geometry = box
         boxNode.position = SCNVector3(x, y, z)
-        
+        // We then add our box node to the root node of the scene. A root node in a scene that defines the coordinate system of the real world rendered by SceneKit.
         sceneView.scene.rootNode.addChildNode(boxNode)
     }
     
@@ -61,7 +51,7 @@ class ViewController: UIViewController {
     }
     
     @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
-        //First, we perform a hit test we specify a .featurePoint result type for the types parameter. The types parameter asks the hit test to search for real-world objects or surfaces detected through the AR session’s processing of the camera image. There are many types of the result type. However, we will focus on just the feature point in this tutorial.
+        //First, we perform a hit test we specify a .featurePoint result type for the types parameter. The types parameter asks the hit test to search for real-world objects or surfaces detected through the AR session’s processing of the camera image.
         let tapLocation = recognizer.location(in: sceneView)
         let hitTestResults = sceneView.hitTest(tapLocation)
         // beacuse ARKit may not always detect a real world object or a surface in the real world.
